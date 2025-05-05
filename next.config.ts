227db712b27a8cd72c7c17f990e3324bcf58416d
mirkhaydarzalyaliev <@MirZal>
2025-05-05 14:11:10 +0300

    Add basePath for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  // Добавляем basePath
+  basePath: '/school_frontend', // Замените на /имя_вашего_репозитория, если оно другое
+  images: {
+    unoptimized: true,
+  },
+  // Другие ваши конфигурации...
 };
 
 export default nextConfig;
